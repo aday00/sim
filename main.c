ddcde85ebf8eeb1bd2cb1002af2697a021caf019
A <a-day@a-vis.(none)>
2011-04-23 17:12:40 -0700

    Limited argument parsing.

--- main.c
+++ main.c
@@ -4,10 +4,33 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "main.h"
 
+void usage(void) {
+  printf("usage\n");
+}
+
 int main(int argc, char** argv)
 {
-  printf("hello\n");
+  int datalen = 0;
+  char *s = NULL;
+  int o;
+
+  while ((o = getopt(argc, argv, "hl:")) != -1) {
+    switch (o) {
+    case 'l':
+      datalen = (int)strtoul(optarg, NULL, 0);
+      break;
+    case 'h':
+    default:
+      usage();
+      exit(1);
+      break;
+    }
+  }
+
+  printf("hello, datalen is %d\n", datalen);
+  return 0;
 }
