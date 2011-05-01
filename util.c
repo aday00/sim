bd79e00c931a292b4c9e12ee0309b556580fa78a
A <a-day@a-vis.(none)>
2011-05-01 13:20:07 -0700

    Adding util.c, which should have been included in this commit: commit 223310aceb867e71aefcabb9d5688cbb52d92575 Author: A <a-day@a-vis.(none)> Date:   Sat Apr 30 21:08:13 2011 -0700
        Starting cl_job.c as a procelain for the OpenCL API.

---
+++ util.c
@@ -0,0 +1,9 @@
+/*
+ * util.c  Copyright 2011 Andrew Schaumberg
+ *
+ * Utility definitions, for either CPU or GPU.
+ */
+
+#ifndef __SIM_USE_GPU__
+int fake_global_id;
+#endif /* __SIM_USE_GPU__ */
